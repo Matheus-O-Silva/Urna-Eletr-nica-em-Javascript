40ff4a38f96e086f1b6734e9383e7c7ca142672f
Matheus <matheuso.silva95@gmail.com>
2021-09-23 11:06:04 -0300

    first_commit

---
+++ etapas.js
@@ -0,0 +1,24 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:38111,
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'prefeito.jpg', legenda: 'Vereador', small:false}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'wiggum.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    }
+]
