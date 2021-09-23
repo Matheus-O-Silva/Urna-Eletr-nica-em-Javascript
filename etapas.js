667d925fcf599401e6f91153dcef556f229a6e3a
Matheus <matheuso.silva95@gmail.com>
2021-09-23 14:51:09 -0300

    selecionar vereador

--- etapas.js
+++ etapas.js
@@ -8,7 +8,7 @@ let etapas = [
                 name: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos:[
-                    {url:'prefeito.jpg', legenda: 'Vereador', small:false}
+                    {url:'prefeito.jpg', legenda: 'Vereador'}
                 ]
             },
             {
@@ -20,5 +20,31 @@ let etapas = [
                 ]
             },
         ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'vice.jpg', legenda: 'Prefeito'},
+                    {url:'mayor1.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'wiggum.png', legenda: 'Prefeito'},
+                    {url:'vice.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
     }
-]
+];
