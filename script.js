c708d37515fc1c339ced3e7ec92cc1754acd907b
Matheus <matheuso.silva95@gmail.com>
2021-09-24 13:26:13 -0300

    confirmar voto

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
@@ -103,10 +104,16 @@ function confirma() {
 
     if(votoBranco === true){
         votoConfirmado = true;
-        console.log("Confirmado como BRANCO");
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
     } else if(numero.length === etapa.numeros){
-        console.log("Confirmando como "+numero);
         votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
     }
 
     if(votoConfirmado){
@@ -115,6 +122,7 @@ function confirma() {
             comecarEtapa();
         } else {
             document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos);
         }
     }
 }
